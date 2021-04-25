89893adadf1a74e1a3143967a9131d3998178226
Luc Saffre <luc.saffre@gmail.com>
2021-04-25 19:11:48 +0300

    fix redirected urls

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from atelier.invlib import setup_from_tasks
 ns = setup_from_tasks(
     globals(), "sphinxfeed",
-    blogref_url="http://luc.lino-framework.org",
+    blogref_url="https://luc.lino-framework.org",
     revision_control_system='git',
     # tolerate_sphinx_warnings=True,
     cleanable_files=[])
